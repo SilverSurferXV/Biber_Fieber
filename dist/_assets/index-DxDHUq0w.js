b837c085484c501ea24ecdcc9c8975833e047a9d
SilverSurferXV <caglar.kuyumcuoglu@googlemail.com>
2026-08-10 22:23:23 +0200

    Create index-DxDHUq0w.js

---
+++ dist/_assets/index-DxDHUq0w.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["_assets/web-C2AMh151.js","_assets/vendor-6HEsgboc.js","_assets/vendor-CIGW-MKW.css"])))=>i.map(i=>d[i]);
+import{_ as p}from"./index-NN9g7wGb.js";import{c5 as r}from"./vendor-6HEsgboc.js";const _=r("App",{web:()=>p(()=>import("./web-C2AMh151.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
